d7731c58e96cf21557addd9c7cdb02dd58c84645
Matt Morris <matt.morris@placewise.com>
2014-09-26 14:38:03 -0600

    add draw method to deck.  define game object with table data structure and deal method

--- sol.rb
+++ sol.rb
@@ -12,8 +12,8 @@ class Deck
     self
   end
 
-  def draw
-    @cards.first
+  def draw(draw_number: 1)
+    @cards.slice! -draw_number, draw_number
   end
 end
 
@@ -38,4 +38,41 @@ class Card
   end
 end
 
+class Solitaire
+  attr_reader :table
+
+  def initialize(draw_number: 3)
+    @draw_number = draw_number
+    @deck = Deck.new.shuffle
+    @table = {
+      foundation: {
+        spades: [], clubs: [], hearts: [], diamonds: []
+      },
+
+      tableau: (1..7).map { [] },
+
+      pile: {
+        stock: [], waste: []
+      }
+    }
+  end
+
+  def deal
+    @table[:tableau].each_with_index do |_, i|
+      (i...@table[:tableau].length).each_with_index do |_, j|
+        card = @deck.draw.first
+        p "[#{i}, #{j}] #{card.rank} of #{card.suit}"
+        @table[:tableau][i + j].push card
+      end
+    end
+    self
+  end
+
+  def draw
+    @deck.draw @draw_number
+  end
+end
+
+# p Solitaire.new.deal.table[:tableau].each_with_index { |column, i| p "column #{i}:"; column.each { |card| p card } }
 # p Deck.new.shuffle.draw
+# p Card.new(2, :Spades).rank
